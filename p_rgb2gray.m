3cb401022d7552b52972f1361b675c2f8a4c33d4
XuJun <nankaimathxujun@gmail.com>
2016-12-29 16:54:26 +0800

    add

--- p_rgb2gray.m
+++ p_rgb2gray.m
@@ -1,10 +1,12 @@
-fpath       =   fullfile('*.png');
+Original_image_dir = 'C:/Users/csjunxu/Desktop/Projects/4000images/';
+fpath       =   fullfile(Original_image_dir, '*.bmp');
 im_dir      =   dir(fpath);
 im_num      =   length(im_dir);
 
 for  i  =  1 : im_num
-    RGB         =  imread(fullfile( im_dir(i).name));
+    RGB         =  imread(fullfile(Original_image_dir, im_dir(i).name));
+    S = regexp(im_dir(i).name, '\.', 'split');
+    fprintf('%s.\n',im_dir(i).name);
     GRAY = rgb2gray(RGB);
-    name = strcat('gray_k',num2str(i),'.png');
-    imwrite(GRAY,name);
+    imwrite(GRAY, [Original_image_dir S{1},'.png']);
 end
