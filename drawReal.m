df6a83fdec34d871d8ac7397a899589703bbd595
Xu Jun <nankaimathxujun@gmail.com>
2019-11-20 09:02:26 +0800

    add

--- drawReal.m
+++ drawReal.m
@@ -1,103 +1,81 @@
 clear;
-TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR2017\1_Results\Real_NoisyImage\';
-TT_fpath = fullfile(TT_Original_image_dir, 'vehicle.png');
+TT_Original_image_dir = 'C:\Users\csjunxu\Desktop\CVPR-2020\VisualCompare\';
+TT_fpath = fullfile(TT_Original_image_dir, '0006_6.png');
 TT_im_dir  = dir(TT_fpath);
 im_num = length(TT_im_dir);
 
-% 6 7 8 10 12 14
+scale = 0.3;
 for i = 1
     fprintf('%s: \n',TT_im_dir(i).name);
     S = regexp(TT_im_dir(i).name, '\.', 'split');
     im = S{1};
-    h = 200;
-    w = 250;
+    h = 150;
+    w = 200;
     s = 50;
     f = 5;
     lr = 2;
+
+%     h = 120;
+%     w = 100;
+%     s = 50;
+%     f = 5;
+%     lr = 1;
     
         %% Noisy
         image = imread(fullfile(TT_Original_image_dir, TT_im_dir(i).name));
         [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
         imname = sprintf('br_Noisy_%s.png',im);
         imwrite(outputimage,imname,'png');
     
         %% BM3D
-        image = imread( sprintf('1_Results/Real_BM3D/BM3D_Real_%s.png',im));
+        image = imread('0006_6_CBM3D_30.89_0.9501.png');
         [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_BM3D_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% WNNM
-        image = imread( sprintf('1_Results/Real_WNNM/WNNM_Real_%s.png',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_WNNM_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% MLP
-        image = imread(sprintf('1_Results/Real_MLP/MLP_Real_10_%s.png',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_MLP_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% CSF
-        image = imread(sprintf('1_Results/Real_CSF/CSF_Real_15_%s.png',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_CSF_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% TRD
-        image = imread(sprintf('1_Results/Real_TRD/TNRD_Real_15_%s.png',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_TRD_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% NeatImage
-        image = imread( sprintf('1_Results/Real_NeatImage/NI_%s.jpg',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_NI_%s.png',im);
-        imwrite(outputimage,imname,'png');
-    
-        %% Noise Clinic
-        image = imread( sprintf('1_Results/Real_NoiseClinic/NC_Real_%s.png',im));
-        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf('br_NC_%s.png',im);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_CBM3D_%s.png',im);
         imwrite(outputimage,imname,'png');
         
-    for para = 0.001:0.001:0.01
-        %% Guided
-        image = imread(sprintf(['1_Results/Real_Guided/Guided_RID_6x6x3x10_' im '_' num2str(para) '.png']));
+        %% GCBD
+        image = imread('0006_6_GCBD_30.73_0.9446.png');
         [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-        imname = sprintf(['br_Guided_RID_6x6x3x10_' im '_' num2str(para) '.png']);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_GCBD_%s.png',im);
         imwrite(outputimage,imname,'png');
-    end
+    
+        %% DnCNN
+        image = imread('0006_6_dncnn+_31.56_0.9575.png');
+        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_dncnn+_%s.png',im);
+        imwrite(outputimage,imname,'png');
+        
+                %% CBDNet
+        image = imread('0006_6_CBDNet_31.06_0.9554.png');
+        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_CBDNet_%s.png',im);
+        imwrite(outputimage,imname,'png');
+    
+        %% N2N
+        image = imread('0006_6_N2N_28.75_0.9067.png');
+        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_N2N_%s.png',im);
+        imwrite(outputimage,imname,'png');
+    
+    
+        %% NeatImage
+        image = imread('0006_6_NI_28.43_0.9126.png');
+        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_NI_%s.png',im);
+        imwrite(outputimage,imname,'png');
+        
+        %% NAC
+        image = imread('0006_6_NAC_31.27_0.9564.png');
+        [ outputimage ] = boxandresize( image, h,w,s, f,lr);
+        outputimage = imresize(outputimage, scale);
+        imname = sprintf('br_NAC_%s.png',im);
+        imwrite(outputimage,imname,'png');
+
 end
-
-% % DSCDL
-% image = imread(sprintf('1_Results/Real_DSCDL/DSCDL_%s.png',im));
-% [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-% imname = sprintf('br_DSCDL_%s.png',im);
-% imwrite(outputimage,imname,'png');
-
-% %% CPSDL
-% image = imread(sprintf('1_Results/Real_CPSDL/CPSDL_%s.png',im));
-% [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-% imname = sprintf('br_CPSDL_%s.png',im);
-% imwrite(outputimage,imname,'png');
-
-% %% PGPD
-% image = imread( sprintf('./PGPD/Real/PGPD_%s.png',im));
-% [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-% imname = sprintf('br_PGPD_%s.png',im);
-% imwrite(outputimage,imname,'png');
-
-% %% WESNR
-% image = imread( sprintf('./WESNR/Real/WESNR_%s.png',im));
-% [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-% imname = sprintf('br_WESNR_%s.png',im);
-% imwrite(outputimage,imname,'png');
-
-% %% TwoPhase
-% image = imread( sprintf('./TwoPhase/Real/TwoPhase_%s.png',im));
-% [ outputimage ] = boxandresize( image, h,w,s, f,lr);
-% imname = sprintf('br_TwoPhase_%s.png',im);
-% imwrite(outputimage,imname,'png');
